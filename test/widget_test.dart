b0cd2a253c929a7800908db5a122b0882f6eb6a8
Mustafa UYSAL <18701031@mersin.edu.tr>
2021-03-01 23:56:02 +0300

    Initial Commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:datingapp/main.dart';
+import 'package:tinder_clone/pages/root_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(RootPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
